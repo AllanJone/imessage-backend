04d8711720d7c3e59340dded3d1cf238ad1250b8
MaouJoe <allanjonejpn@gmail.com>
2020-10-23 07:07:29 +0900

    Added db model

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Import
 import express from 'express'
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
 import pusher from 'pusher'
 import cors from 'cors'
 
@@ -13,11 +13,16 @@ app.use(cors())
 app.use(express.json())
 
 //Db Config
+const mongoURI = '';
 
+mongoose.connect(mongoURI,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 
 //API Routes
 app.get('/',(req,res)=>res.status(200).send('Hello World!'))
-
 
 //Listen
 app.listen(port,()=>console.log(`'Listening on localhost:${port}`))
