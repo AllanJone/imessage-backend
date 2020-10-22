09e21362214e8a185d22deafafcec56530c2aef6
MaouJoe <allanjonejpn@gmail.com>
2020-10-23 07:16:58 +0900

    Connected to Database

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // Import
 import express from 'express'
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 import pusher from 'pusher'
 import cors from 'cors'
+
+import mongoData from './mongoData.js'
 
 // App Config
 const app = express()
@@ -13,7 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 //Db Config
-const mongoURI = '';
+const mongoURI = 'mongodb+srv://admin:41qElIJOV3849Bk5@cluster0.pkv8n.mongodb.net/imessageDB?retryWrites=true&w=majority';
 
 mongoose.connect(mongoURI,{
     useCreateIndex:true,
@@ -21,8 +23,12 @@ mongoose.connect(mongoURI,{
     useUnifiedTopology:true
 })
 
+mongoose.connection.once('open',()=>{
+    console.log('DB Connected')
+})
+
 //API Routes
 app.get('/',(req,res)=>res.status(200).send('Hello World!'))
 
 //Listen
-app.listen(port,()=>console.log(`'Listening on localhost:${port}`))
+app.listen(port,()=>console.log(`Listening on localhost:${port}`))
